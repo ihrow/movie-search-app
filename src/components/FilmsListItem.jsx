36c5c8889037c65a5c62c0172302b39243282386
igor <igornotfound@gmail.com>
2023-03-31 01:21:50 +0200

    change animation

--- src/components/FilmsListItem.jsx
+++ src/components/FilmsListItem.jsx
@@ -18,10 +18,10 @@ const FilmsListItem = ({ idx, film }) => {
 
   return (
     <motion.div 
-      className="w-full rounded-lg shadow-lg bg-gray-800 border-gray-700 my-5 md:my-8"
-      variants={fadeIn(idx % 2 === 0 ? "right" : "left", "spring", 0.5, 0.4)}
+      className="w-full rounded-lg shadow-lg bg-gray-800 border-gray-700 my-5 md:my-8 cursor-pointer"
+      variants={fadeIn(idx % 2 === 0 ? "right" : "left", "spring", 0.2, 0.5)}
       initial="hidden"
-      animate="show"
+      whileInView="show"
       whileHover={{ scale: 1.05 }}
       onClick={() => {
         navigate(`/${film.imdbID}`)
