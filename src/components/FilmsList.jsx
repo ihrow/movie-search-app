a4832397f90a60ec953f30bf2f74eefeaecbe3e7
igor <igornotfound@gmail.com>
2023-04-03 00:45:46 +0200

    fixed observer

--- src/components/FilmsList.jsx
+++ src/components/FilmsList.jsx
@@ -7,25 +7,27 @@ export function FilmsList({ setPage }) {
   const { films, loading, error } = useSelector((state) => state.films);
 
   useEffect(() => {
+    if (!intersectionRef.current) return;
     const observer = new IntersectionObserver(([entry]) => {
       if (entry.isIntersecting) {
         setPage((prevPage) => prevPage + 1);
       }
     });
-    setTimeout(() => {
-      observer.observe(intersectionRef.current);
-    }, 1000);
+
+    observer.observe(intersectionRef.current);
+
     document.addEventListener("DOMContentLoaded", () => {
       return () => {
         observer.disconnect();
       };
     });
-  }, []);
+  }, [intersectionRef.current]);
 
   if (error) {
     return (
       <h3 className="mt-5 text-2xl font-bold text-red-600">
         {error === "Incorrect IMDb ID." ? "Enter movie name." : error}
+        <div className="h-10 w-full" ref={intersectionRef} />
       </h3>
     );
   }
@@ -34,11 +36,11 @@ export function FilmsList({ setPage }) {
     <div className="flex flex-row flex-wrap items-center justify-center">
       {!error && <MasonryLayout films={films} />}
       {loading && (
-        <div className="h-[50px] w-full text-center text-xl font-bold text-black">
+        <div className="h-8 w-full text-center text-2xl font-bold text-black">
           Loading...
         </div>
       )}
-      <div className="w-full" ref={intersectionRef} />
+      <div className="h-10 w-full" ref={intersectionRef} />
     </div>
   );
 }
