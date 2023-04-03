f929189f950357ed0c1b97e29be30d89bada369b
igor <igornotfound@gmail.com>
2023-04-03 23:14:43 +0200

    added scrollToTop button

--- src/components/FilmsList.jsx
+++ src/components/FilmsList.jsx
@@ -1,10 +1,28 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { MasonryLayout } from "./MasonryLayout";
+import { AiOutlineArrowUp } from "react-icons/ai";
 
 export function FilmsList({ setPage }) {
+  const [isButtonToTopVisible, setIsButtonToTopVisible] = useState(false);
   const intersectionRef = useRef();
   const { films, loading, error } = useSelector((state) => state.films);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
+  const onScroll = () => {
+    if (window.scrollY > 1000) {
+      setIsButtonToTopVisible(true);
+    } else {
+      setIsButtonToTopVisible(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+  },[]);
 
   useEffect(() => {
     if (!intersectionRef.current) return;
@@ -40,6 +58,15 @@ export function FilmsList({ setPage }) {
       {loading && (
         <div className="h-8 w-full text-center text-2xl font-bold text-black">
           Loading...
+        </div>
+      )}
+      {isButtonToTopVisible && (
+        <div className="fixed bottom-0 left-100 w-full text-2xl font-bold text-black mb-6">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded-full"
+            onClick={scrollToTop}
+          >
+            <AiOutlineArrowUp fontSize={32} fontWeight='bold' />
+          </button>
         </div>
       )}
       <div className="h-10 w-full" ref={intersectionRef} />
